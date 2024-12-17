88727c37573f34a843288a152407a1c313e8fef1
vanshika-dargan-18 <vanshika.dargan@vonage.com>
2024-12-17 15:27:37 +0530

    Revert "Admin shubh"

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,26 @@
 <template>
-  <div id="app">
-    <Login />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import Login from './components/LoginPag.vue';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    Login
+    HelloWorld
   }
-};
+}
 </script>
 
 <style>
-/* Add global styles (if needed) */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
