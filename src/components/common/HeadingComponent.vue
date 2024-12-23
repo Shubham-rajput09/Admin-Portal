88bbb4fb55856855c39d2db01a6672be2d564999
vanshika-dargan-18 <vanshika.dargan@vonage.com>
2024-12-23 15:51:21 +0530

    Added new empty component files

---
+++ src/components/common/HeadingComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Heading Component</div>
+</template>
+
+<script>
+export default {
+  name: 'HeadingComponent',
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/common/PrimaryButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Primary Button</div>
+</template>
+
+<script>
+export default {
+  name: 'PrimaryButton',
+};
+</script>
+
+<style scoped></style>
