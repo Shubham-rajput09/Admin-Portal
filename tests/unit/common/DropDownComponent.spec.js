21208263bb265f6a9de1b70a2bebabca8be61bfe
Divyashree M N <divyashreem.n@vonage.com>
2025-01-28 17:52:43 +0530

    code-update:moving-testing-components-to-common-directory

--- tests/unit/common/DropDownComponent.spec.js
+++ tests/unit/common/DropDownComponent.spec.js
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils';
-import DropDownComponent from '@/components/common/DropDownComponent';
+import DropDownComponent from '@/components/common/DropdownComponent.vue';
 
 describe('DropdownComponent.vue', () => {
   const dropDownTitle = 'AddBulk';
